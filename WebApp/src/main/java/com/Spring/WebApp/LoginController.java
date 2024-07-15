6630fbe91996923e38f005552bc644be4216ea00
Alice Gonzalez <144973566+AliceGonzalez@users.noreply.github.com>
2024-07-14 23:49:06 -0400

    Adding Home and Login Controllers

---
+++ WebApp/src/main/java/com/Spring/WebApp/LoginController.java
@@ -0,0 +1,15 @@
+package com.Spring.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "Login Page working...";
+    }
+}

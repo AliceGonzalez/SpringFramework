d64db682f46cde99e92b904518827b19425c8392
Alice Gonzalez <144973566+AliceGonzalez@users.noreply.github.com>
2024-07-14 23:40:11 -0400

    Added about me path

--- WebApp/src/main/java/com/Spring/WebApp/HomeController.java
+++ WebApp/src/main/java/com/Spring/WebApp/HomeController.java
@@ -2,13 +2,20 @@ package com.Spring.WebApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-    //METHOD
-    RequestMapping("/")
 
+    //METHOD
+    @RequestMapping("/")
     public String greet(){
         return "Welcome to my Spring WebApp!!!";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "This is my about me page!!";
     }
 }
